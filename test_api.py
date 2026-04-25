19346c6c3c859907b1a93adb8b77c912d0733b7e
kristina-pachi <kristina.pachi.04@bk.ru>
2026-04-25 12:51:38 +0300

    Дополнены тесты api

--- test_api.py
+++ test_api.py
@@ -3,6 +3,7 @@ from unittest.mock import patch, MagicMock
 from api import get_weather
 
 
+# тест на успешней ответ
 def test_get_weather_success():
 
     # фейковый объект ответа requests.get()
@@ -27,3 +28,34 @@ def test_get_weather_success():
         "Скорость ветра": 3.2,
         "Влажность": 80
     }
+
+# тест с неверным json ответом
+def test_get_weather_bad_json():
+
+    mock_response = MagicMock()
+    mock_response.status_code = 200
+    mock_response.json.return_value = {}  # пустой JSON
+
+    with patch("api.requests.get", return_value=mock_response):
+        result = get_weather(55.75, 37.61, "FAKE_KEY")
+
+    assert result is None
+
+# ошибка сервера
+def test_get_weather_api_error():
+
+    mock_response = MagicMock()
+    mock_response.status_code = 404  # ошибка API
+
+    with patch("api.requests.get", return_value=mock_response):
+        result = get_weather(55.75, 37.61, "FAKE_KEY")
+
+    assert result is None
+
+# ошибка сети
+def test_get_weather_exception():
+
+    with patch("api.requests.get", side_effect=Exception("ошибка сети")):
+        result = get_weather(55.75, 37.61, "FAKE_KEY")
+
+    assert result is None
